49d8b9154e870bf31cdae5b1a375f30e98d84717
nayarawatanuki <nayarawatanuki@gmail.com>
2021-11-22 13:49:48 -0300

    test device

--- src/frontend/pages/KidAcess/Task3/index.js
+++ src/frontend/pages/KidAcess/Task3/index.js
@@ -134,6 +134,13 @@ function Task3() {
     history.push('/Tasks');
   }
 
+  const isTouchDevice = () => {
+    if ("ontouchstart" in window) {
+      return true;
+    }
+    return false;
+  };
+
   return (
     <App>
       <nav className="navbar navbar-light bg-light">
@@ -182,9 +189,8 @@ function Task3() {
                     <Label id="emotion1" name="emotion1" 
                       style={{marginRight: '4%', border: '5px double #ae8625', background: '-webkit-gradient(linear, left top, center top, from(#ae8625), to(#e6bc53)'}}
                       value={task.response1}
-                      onTouchStart={true}
                       draggable={true} 
-                      onDragStart={(e)=> {drag(e); setResponse(task.response1)}}
+                      onDragStart={(e)=> {isTouchDevice; drag(e); setResponse(task.response1)}}
                       >{task.response1}
                     </Label>
                   </Draggable>
