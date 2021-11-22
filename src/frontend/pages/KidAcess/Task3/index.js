5a1af4e6d32520db2d5ec210091d82a3ebb48a78
nayarawatanuki <nayarawatanuki@gmail.com>
2021-11-22 16:22:16 -0300

    drag

--- src/frontend/pages/KidAcess/Task3/index.js
+++ src/frontend/pages/KidAcess/Task3/index.js
@@ -178,13 +178,13 @@ function Task3() {
                 </div>
 
                 <div className="row" style={{marginTop: '5%', justifyContent: 'center'}}>
-                  <Draggable cancel=".btn">
+                  <Draggable cancel=".btn" onDragStart={(e)=> {drag(e); setResponse(task.response1)}}>
                   <button id="emotion1" name="emotion1" className='btn'
                     style={{marginRight: '4%', border: '5px double #ae8625', background: '-webkit-gradient(linear, left top, center top, from(#ae8625), to(#e6bc53)'}}
                     value={task.response1}
                    
                     
-                    onDragStart={(e)=> {drag(e); setResponse(task.response1)}}
+                    
                     >{task.response1}
                   </button>
                   </Draggable>
