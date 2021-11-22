fb25e45aacbaf33da97e6dbaa764c3c57d4b499b
nayarawatanuki <nayarawatanuki@gmail.com>
2021-11-22 14:07:11 -0300

    touch test

--- src/frontend/pages/KidAcess/Task3/index.js
+++ src/frontend/pages/KidAcess/Task3/index.js
@@ -189,9 +189,9 @@ function Task3() {
                       style={{marginRight: '4%', border: '5px double #ae8625', background: '-webkit-gradient(linear, left top, center top, from(#ae8625), to(#e6bc53)'}}
                       value={task.response1}
                       draggable={true} 
-                      onTouchStart={true}
+                      
                       onTouchMove={true}
-                      onPress={true}
+                      
                       onDragStart={(e)=> {drag(e); setResponse(task.response1)}}
                       >{task.response1}
                     </Label>
