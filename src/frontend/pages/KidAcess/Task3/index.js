41933b443f986a5a71224fa02b85bb4febcbc807
nayarawatanuki <nayarawatanuki@gmail.com>
2021-11-22 13:59:30 -0300

    touch

--- src/frontend/pages/KidAcess/Task3/index.js
+++ src/frontend/pages/KidAcess/Task3/index.js
@@ -185,7 +185,6 @@ function Task3() {
                 </div>
 
                 <div className="row" style={{marginTop: '5%', justifyContent: 'center'}}>
-                  <Draggable> 
                     <Label id="emotion1" name="emotion1" 
                       style={{marginRight: '4%', border: '5px double #ae8625', background: '-webkit-gradient(linear, left top, center top, from(#ae8625), to(#e6bc53)'}}
                       value={task.response1}
@@ -196,7 +195,6 @@ function Task3() {
                       onDragStart={(e)=> {drag(e); setResponse(task.response1)}}
                       >{task.response1}
                     </Label>
-                  </Draggable>
 
                   <Label id="emotion2" name="emotion2" style={{marginRight: '4%', border: '5px double #ae8625', background: '-webkit-gradient(linear, left top, center top, from(#ae8625), to(#e6bc53)'}}
                     value={task.response2} 
