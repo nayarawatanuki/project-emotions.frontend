bb0b8c8523b161cd136d42029170898ea11e9552
nayarawatanuki <nayarawatanuki@gmail.com>
2021-11-22 10:40:09 -0300

    drag unchance

--- src/frontend/pages/KidAcess/Task3/index.js
+++ src/frontend/pages/KidAcess/Task3/index.js
@@ -80,8 +80,7 @@ function Task3() {
 
   async function drag(e) {
     await e.preventDefault();
-    //e.dataTransfer.effectAllowed == 'move';
-    //e.dataTransfer.setData("text/html", e.target.id);
+    e.dataTransfer.setData("text/html", e.target.id);
   }
 
   async function drop(e) {
