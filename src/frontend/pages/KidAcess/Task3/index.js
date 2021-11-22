dd9fb52d71db74bbadc50bddabf41ff9551a1417
nayarawatanuki <nayarawatanuki@gmail.com>
2021-11-22 10:24:39 -0300

    drag config

--- src/frontend/pages/KidAcess/Task3/index.js
+++ src/frontend/pages/KidAcess/Task3/index.js
@@ -79,7 +79,8 @@ function Task3() {
   }
 
   async function drag(e) {
-    await e.dataTransfer.effectAllowed == 'move';
+    await e.preventDefault();
+    e.dataTransfer.effectAllowed == 'move';
     e.dataTransfer.setData("text/html", e.target.id);
   }
 
