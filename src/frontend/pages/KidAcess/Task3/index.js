c37334dd5594b6edee72797dbb459a0db06f33cb
nayarawatanuki <nayarawatanuki@gmail.com>
2021-11-22 12:20:34 -0300

    test draggable

--- src/frontend/pages/KidAcess/Task3/index.js
+++ src/frontend/pages/KidAcess/Task3/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useLayoutEffect } from 'react';
 import swal from '@sweetalert/with-react'
+import { Draggable } from 'react-beautiful-dnd';
 import api from '../../../services/api';
 
 import '../../../global/styles/fireworks/style.css';
@@ -177,14 +178,16 @@ function Task3() {
                 </div>
 
                 <div className="row" style={{marginTop: '5%', justifyContent: 'center'}}>
-                  <Label id="emotion1" name="emotion1" 
-                  style={{marginRight: '4%', border: '5px double #ae8625', background: '-webkit-gradient(linear, left top, center top, from(#ae8625), to(#e6bc53)'}}
-                    value={task.response1}
-                    draggable={true} 
-                    onTouchMove={this.draggable = true}
-                    onDragStart={(e)=> {drag(e); setResponse(task.response1)}}
-                    >{task.response1}
-                  </Label>
+                  <Draggable> 
+                    <Label id="emotion1" name="emotion1" 
+                      style={{marginRight: '4%', border: '5px double #ae8625', background: '-webkit-gradient(linear, left top, center top, from(#ae8625), to(#e6bc53)'}}
+                      value={task.response1}
+                      draggable={true} 
+                      onTouchMove={this.draggable = true}
+                      onDragStart={(e)=> {drag(e); setResponse(task.response1)}}
+                      >{task.response1}
+                    </Label>
+                  </Draggable>
 
                   <Label id="emotion2" name="emotion2" style={{marginRight: '4%', border: '5px double #ae8625', background: '-webkit-gradient(linear, left top, center top, from(#ae8625), to(#e6bc53)'}}
                     value={task.response2} 
