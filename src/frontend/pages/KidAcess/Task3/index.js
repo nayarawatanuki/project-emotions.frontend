1408b2d62b8a04a56c700b950ff954b94cc3dbcb
nayarawatanuki <nayarawatanuki@gmail.com>
2021-11-22 22:09:02 -0300

    test2

--- src/frontend/pages/KidAcess/Task3/index.js
+++ src/frontend/pages/KidAcess/Task3/index.js
@@ -191,7 +191,7 @@ function Task3() {
                     style={{marginRight: '4%', border: '5px double #ae8625', background: '-webkit-gradient(linear, left top, center top, from(#ae8625), to(#e6bc53)'}}
                     value={task.response1}
                     draggable={true}
-                    onTouchStart={...handlers}
+                    onTouchStart={handlers}
                     onDragStart={(e)=> {drag(e); setResponse(task.response1)}}
                     >{task.response1}
                   </Label>
