b1b6f6a9d73e728361bfb6622612a734b6f21ee9
nayarawatanuki <nayarawatanuki@gmail.com>
2021-11-23 03:20:06 -0300

    ajust margintop

--- src/frontend/pages/KidAcess/Task3/index.js
+++ src/frontend/pages/KidAcess/Task3/index.js
@@ -177,7 +177,7 @@ function Task3() {
                   />
                 </div>
                 
-                <div className='row' style={{marginTop: '7%', justifyContent: 'center'}}>
+                <div className='row' style={{marginTop: '10%', justifyContent: 'center'}}>
                   <Label id="emotion1" name="emotion1" 
                     style={{marginRight: '6%', border: '5px double #ae8625', background: '-webkit-gradient(linear, left top, center top, from(#ae8625), to(#e6bc53)'}}
                     value={task.response1}
