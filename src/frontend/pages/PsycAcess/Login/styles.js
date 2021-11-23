3390d0941f72496f1ec0934632116767c884d282
nayarawatanuki <nayarawatanuki@gmail.com>
2021-11-23 04:31:19 -0300

    ajust

--- src/frontend/pages/PsycAcess/Login/styles.js
+++ src/frontend/pages/PsycAcess/Login/styles.js
@@ -17,7 +17,7 @@ export const Container = styled.div`
 `;
 
 export const Content = styled.div`
-    width: auto fit-content;
+    width: fit-content;
     height: auto 100%;
     margin: auto;
     display: center;
